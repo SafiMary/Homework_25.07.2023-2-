9714e195857a181e0aeeb436e7edef19c8e29de8
Мария Сафи <safim@yandex.ru>
2023-08-20 15:10:48 +0500

    Add project files.

---
+++ Homework_25.07.2023(2)/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Homework_25._07._2023_2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.butloadImg = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.butSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butloadImg
+            // 
+            this.butloadImg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butloadImg.Location = new System.Drawing.Point(26, 405);
+            this.butloadImg.Name = "butloadImg";
+            this.butloadImg.Size = new System.Drawing.Size(318, 29);
+            this.butloadImg.TabIndex = 1;
+            this.butloadImg.Text = "Загрузить картинку";
+            this.butloadImg.UseVisualStyleBackColor = true;
+            this.butloadImg.Click += new System.EventHandler(this.butloadImg_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // butSave
+            // 
+            this.butSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butSave.Location = new System.Drawing.Point(376, 405);
+            this.butSave.Name = "butSave";
+            this.butSave.Size = new System.Drawing.Size(302, 29);
+            this.butSave.TabIndex = 1;
+            this.butSave.Text = "Сохранить ";
+            this.butSave.UseVisualStyleBackColor = true;
+            this.butSave.Click += new System.EventHandler(this.butSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 653);
+            this.Controls.Add(this.butSave);
+            this.Controls.Add(this.butloadImg);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button butloadImg;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button butSave;
+    }
+}
+
---
+++ Homework_25.07.2023(2)/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Homework_25._07._2023_2_
+{
+    public partial class Form1 : Form
+    {
+        Point mouseDownLocation;
+        TextBox T;
+        PictureBox pictureBox1 = new PictureBox();
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseDoubleClick += pictureBox1_MouseDoubleClick;
+
+        }
+        private void butloadImg_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            pictureBox1.Size = new Size(300, 300);
+            pictureBox1.Location = new Point(20, 20);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            Controls.Add(pictureBox1);
+            openFileDialog1.ShowDialog();
+            pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);//картинка по выбору пользователя
+
+            //pictureBox1.Image = img1;// Вставили картинку в пиктер бокс
+
+        }
+
+        private void butSave_Click(object sender, EventArgs e)
+        {
+             //сохранение картинки
+            Bitmap bmpSave = new Bitmap(pictureBox1.DisplayRectangle.Width, pictureBox1.DisplayRectangle.Height);
+            pictureBox1.DrawToBitmap(bmpSave, pictureBox1.DisplayRectangle);
+            saveFileDialog1.FileName = "myPicture";
+            saveFileDialog1.DefaultExt = "bmp";//Расширение имени файла по умолчанию
+            saveFileDialog1.AddExtension = true;
+            saveFileDialog1.CreatePrompt = true;//если указан не существующий файл, то будет отображаться сообщение о его создании
+            saveFileDialog1.OverwritePrompt = false; //если указан существующий файл, то будет отображаться сообщение о том, что файл не будет перезаписан
+            saveFileDialog1.InitialDirectory = @"D:/";//устанавливает каталог, который отображается при первом вызове окна
+            saveFileDialog1.Title = "Сохраните свой файл здесь";
+            saveFileDialog1.Filter = "Image files (*.bmp)|*.bmp|All files (*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                bmpSave.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                MessageBox.Show("Файл сохранен");
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)//определение координат мыши при нажатии
+        {
+            mouseDownLocation = new Point(e.X, e.Y);
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)//создать комментарий на фото
+        {
+            T = new TextBox();
+            T.Location = new Point(mouseDownLocation.X, mouseDownLocation.Y);
+            T.Parent = pictureBox1;
+            T.Height = 30;
+            T.Width = 100;
+            pictureBox1.Controls.Add(T);
+        }
+
+       
+    }
+
+}
+
+
+
